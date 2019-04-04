2e0b16bfd1d1361431df6a0f123268d8f6666724
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-04-04 11:20:33 +0800

    npu_monitor: decrease write sys node.
    Change-Id: Idce40317b2db71bf768c977b88411d6ec534ed31
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@
 
 #define SYS_TEMPERATURE_NODE "/sys/class/thermal/thermal_zone0/temp"
 #define SYS_GPU_POLICY_NODE "/sys/devices/platform/ff9a0000.gpu/power_policy"
-#define THRESHOLD_TEMPERATURE (long)0
+#define THRESHOLD_TEMPERATURE (long)10000
 #define SLEEP_SECOND_TIME 3
 
-#define DEBUG 1
+#define DEBUG 0
+static int gpuStatus = 0;
 
 static void simple_echo(char *path, char *s)
 {
@@ -33,9 +34,9 @@ static void simple_echo(char *path, char *s)
     close(fd);
 }
 
-int simple_cat(char *buf, size_t bufsize) {
+int simple_cat(char *path, char *buf, size_t bufsize) {
     int fd = 0;
-    fd = open (SYS_TEMPERATURE_NODE, O_RDONLY);
+    fd = open (path, O_RDONLY);
     if (fd < 0) {
         ALOGE("can't open node");
         close(fd);
@@ -60,26 +61,50 @@ int simple_cat(char *buf, size_t bufsize) {
     return 0;
 }
 
+/**
+ * policy0: coarse_demand, policy1: always_on
+ * */
+int changeGPUPolicy(int policy) {
+    if(DEBUG) ALOGD("==== gpuStatus: %d, policy: %d ====", gpuStatus, policy);
+    if ((policy == 0) && (gpuStatus == 1)) {
+        simple_echo(SYS_GPU_POLICY_NODE, "coarse_demand");
+        gpuStatus = 0;
+        ALOGD("==== coarse_demand ====");
+        return 0;
+    }
+    if ((policy == 1) && (gpuStatus == 0)) {
+        simple_echo(SYS_GPU_POLICY_NODE, "always_on");
+        gpuStatus = 1;
+        ALOGD("==== always_on ====");
+        return 0;
+    }
+    return -1;
+}
+
 int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
 
     ALOGD("==== NPU Monitor start ====");
+    int sizeMaxOfStatus = strlen("[coarse_demand] always_on");
+    char strGpuStatus[sizeMaxOfStatus];
+    if (simple_cat(SYS_GPU_POLICY_NODE, strGpuStatus, sizeMaxOfStatus) == 0) {
+        ALOGD("==== gpu node = %s ====", strGpuStatus);
+        gpuStatus = strstr(strGpuStatus, "\[always_on\]")? 1 : 0;
+        if (DEBUG) ALOGD("current gpu policy(%d): %s", gpuStatus, strGpuStatus);
+    }
     char strTemperature[sizeof(long)];
     while(1) {
-        if (simple_cat(strTemperature, sizeof(long)) == 0) {
+        if (simple_cat(SYS_TEMPERATURE_NODE, strTemperature, sizeof(long)) == 0) {
             if(DEBUG) ALOGD(" start monitoring... temp is %s", strTemperature);
             long temperature = atol(strTemperature);
             if (temperature > THRESHOLD_TEMPERATURE) {
-                simple_echo(SYS_GPU_POLICY_NODE, "coarse_demand");
-                if(DEBUG) ALOGD("==== coarse_demand ====");
+                changeGPUPolicy(0);
             } else {
-                simple_echo(SYS_GPU_POLICY_NODE, "always_on");
-                if(DEBUG) ALOGD("==== always_on ====");
+                changeGPUPolicy(1);
             }
         }
-
-        if(DEBUG) ALOGD("sleeping ...");
+        //if(DEBUG) ALOGD("sleeping ...");
         sleep(SLEEP_SECOND_TIME);
     }
 
